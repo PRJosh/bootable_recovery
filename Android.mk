bd5a4b6a014ee484bc6e9ff9a281f548f7234e10
Josue Rivera <prbassplayer@gmail.com>
2015-10-12 23:16:45 +0200

    Revert "recovery: Add lz4 libs"
    This reverts commit d5a03fc8e722b5a6503b4a7cc847ed01a16a38cf.
    
    Change-Id: Iab70e8058594855d615c5dc7894e13c24c38ef93

--- Android.mk
+++ Android.mk
@@ -112,7 +112,6 @@ ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
     LOCAL_CFLAGS += -DUSE_EXT4
     LOCAL_C_INCLUDES += system/extras/ext4_utils
     LOCAL_SHARED_LIBRARIES += libext4_utils
-    LOCAL_STATIC_LIBRARIES += liblz4-static
 endif
 ifneq ($(wildcard external/libselinux/Android.mk),)
     TWHAVE_SELINUX := true
@@ -130,7 +129,6 @@ ifeq ($(TWHAVE_SELINUX), true)
         LOCAL_CFLAGS += -DUSE_EXT4
         LOCAL_C_INCLUDES += system/extras/ext4_utils
         LOCAL_SHARED_LIBRARIES += libext4_utils
-        LOCAL_STATIC_LIBRARIES += liblz4-static
     endif
 endif
 
